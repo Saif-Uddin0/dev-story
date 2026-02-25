c3dbe917a31206d8220fe597ab4930613965a2ee
Saif Uddin <mahirr.846@gmail.com>
2026-02-26 00:06:31 +0600

    added the multiple layout

---
+++ src/app/dashboard/add-story/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title';
+import React from 'react';
+
+const AddStory = () => {
+    return (
+        <div>
+            <Title>this is a add story page</Title>
+        </div>
+    );
+};
+
+export default AddStory;
